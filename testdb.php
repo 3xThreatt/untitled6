4c96fd0d7e06e2b56943e2e098a0430e495f7d6c
3xThreatt <74078818+3xThreatt@users.noreply.github.com>
2020-11-07 15:29:58 +1300

    Add files via upload

--- testdb.php
+++ testdb.php
@@ -4,22 +4,47 @@ $username = "pl5olm9uaqn9xhli";
 $password = "o7mk03t99ecbrw8c";
 $dbname = "j4hxbdarc69h9toz";
 
-// Create connection
 $link = mysqli_connect($servername, $username, $password, $dbname);
 // Check connection
 // Check connection
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
- 
-// Attempt insert query execution
-$sql = "INSERT INTO persons (first_name, last_name, email) VALUES ('Peter', 'Parker', 'peterparker@mail.com')";
-if(mysqli_query($link, $sql)){
-    echo "Records inserted successfully.";
-} else{
-    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+
+
+$statusMsg = '';
+
+// File upload path
+$targetDir = "uploads/";
+$fileName = basename($_FILES["file"]["name"]);
+$targetFilePath = $targetDir . $fileName;
+$fileType = pathinfo($targetFilePath,PATHINFO_EXTENSION);
+
+if(isset($_POST["submit"]) && !empty($_FILES["file"]["name"])){
+    // Allow certain file formats
+    $allowTypes = array('jpg','png','jpeg','gif','pdf');
+    if(in_array($fileType, $allowTypes)){
+        // Upload file to server
+        if(move_uploaded_file($_FILES["file"]["tmp_name"], $targetFilePath)){
+            // Insert image file name into database
+            $insert = $db->query("INSERT into banners (file_name, uploaded_on) VALUES ('".$fileName."', NOW())");
+            if($insert){
+                $statusMsg = "The file ".$fileName. " has been uploaded successfully.";
+            }else{
+                $statusMsg = "File upload failed, please try again.";
+            } 
+        }else{
+            $statusMsg = "Sorry, there was an error uploading your file.";
+        }
+    }else{
+        $statusMsg = 'Sorry, only JPG, JPEG, PNG, GIF, & PDF files are allowed to upload.';
+    }
+}else{
+    $statusMsg = 'Please select a file to upload.';
 }
- 
+
+// Display status message
+echo $statusMsg;
 // Close connection
 mysqli_close($link);
 ?>
