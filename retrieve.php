04a73f089495b0b5f048746785a9897eb7100260
3xThreatt <74078818+3xThreatt@users.noreply.github.com>
2020-11-07 15:52:37 +1300

    Add files via upload

--- retrieve.php
+++ retrieve.php
@@ -1,11 +1,11 @@
 <?php
-
+include(config.php)
 $sql = "select name from images where id=1";
 $result = mysqli_query($con,$sql);
 $row = mysqli_fetch_array($result);
 
 $image = $row['name'];
-$image_src = "uploads/Banner1.jpg".$image;
+$image_src = "Banner1.jpg";
 
 ?>
 <img src='<?php echo $image_src;  ?>' >
